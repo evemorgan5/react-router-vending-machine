1b7b6644a3923cbcf76bed32f56657d296a1188d
Eve Morgan <evemorgan27@gmail.com>
2022-07-18 13:35:08 -0700

    updated snacks list

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -3,18 +3,16 @@ import { Link } from "react-router-dom";
 
 function VendingMachine() {
   return (
-  <div className="vending-machine">
+  <div className="VendingMachine">
     <h2>
       Welcome to Alex and Eve's Vending Machine!
     </h2>
     <img src="https://thumbs.dreamstime.com/b/vending-machine-simple-icon-vending-machine-filled-outl[…]on-vending-clipart-isolated-white-background-124194022.jpg"
     alt="vending machine image"/>
-    <div className="machine-snacks">
-    <ul>
-      <li><Link to="/coffee">Coffee</Link></li>
-      <li><Link to="/chocolate">Chocolate</Link></li>
-      <li><Link to="/water">Water</Link></li>
-    </ul>
+    <div className="MachineSnacks">
+      <h2><Link to="/coffee">Coffee</Link></h2>
+      <h2><Link to="/chocolate">Chocolate</Link></h2>
+      <h2><Link to="/water">Water</Link></h2>
     </div>
   </div>
   );
